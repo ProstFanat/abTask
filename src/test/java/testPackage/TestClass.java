b920946222c4f49395c09b7b2e2b6c73034fc663
Adrian <adrianboyshuk2001@gmail.com>
2021-05-05 13:36:19 +0300

    update

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -7,6 +7,9 @@ import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 import resources.*;
 
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
 public class TestClass {
 
     static WebDriver driver;
@@ -14,8 +17,10 @@ public class TestClass {
 
     @BeforeClass
     private void beforeAll(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+//        driver = new ChromeDriver();
+        open("http://google.com");
+        driver = getWebDriver();
         basePage = new BasePage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
